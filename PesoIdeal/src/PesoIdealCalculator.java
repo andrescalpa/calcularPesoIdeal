e0c577f5b36db3cea9414c4e4ecb5a564404eb7f
andrescalpa <andrescalpa7@gmail.com>
2016-09-07 23:09:21 -0500

    VERSION 3

---
+++ PesoIdeal/src/PesoIdealCalculator.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+
+public class PesoIdealCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String genero = "";
+		
+		do{
+			genero = JOptionPane.showInputDialog("Introduzca genero (H/M), por favor.");
+		}while(genero.equalsIgnoreCase("H")==false && genero.equalsIgnoreCase("M")==false);
+		
+		int altura=Integer.parseInt(JOptionPane.showInputDialog("Introduce tu altura en cm"));
+		int pesoIdeal=0;
+		if(genero.equalsIgnoreCase("H")){
+			pesoIdeal = altura-110;
+		}
+		
+		else if(genero.equalsIgnoreCase("M")){
+			pesoIdeal=altura-120;
+		}
+		
+		System.out.println("Tu peso ideal es " + pesoIdeal + "kg.");
+
+	}
+
+}
